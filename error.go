80b3c5eae2ff202dae4593bd38eb699d8c967b6e
Alex Caepio <mjbcaepio@gmail.com>
2022-01-17 07:05:31 +0300

    first commit

---
+++ error.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type AppError struct {
+	Err    error
+	Custom string
+	Field  int
+}
+
+func (e *AppError) Error() string {
+	return e.Err.Error()
+}
+
+func m() error {
+	return &AppError{
+		Err:    fmt.Errorf("my error"),
+		Custom: "value here",
+		Field:  89,
+	}
+}
+
+func main() {
+	err := m()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+}
